467304cc80e33a4341119bbb8f37ad715c6ea7cf
BenHurJF <beiujeffer@hotmail.com>
2023-10-31 18:04:11 -0300

    subindo projeto cypress estrutura básica

---
+++ cypress/e2e/testeExemplo.cy.js
@@ -0,0 +1,29 @@
+describe('template spec', () => {
+  it('Teste 1', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 2', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 3', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 4', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 5', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 6', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+  it('Teste 7', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
